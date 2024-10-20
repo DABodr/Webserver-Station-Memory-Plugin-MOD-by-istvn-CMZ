7f341e1442572df245a9515cdad1e0aa2c12d271
RSAP <46652797+DABodr@users.noreply.github.com>
2024-10-20 10:36:35 +0200

    Update Station-Memory.js
    Charleville stations list

--- StationMemory/Station-Memory.js
+++ StationMemory/Station-Memory.js
@@ -1,26 +1,26 @@
 // CONFIG //
 
 var presets = [
-    { country: 'SVK', PI: '5218', format: 'png', freq: 101.8 }, // VLNA
-    { country: 'AUT', PI: 'A203', format: 'png', freq: 99.9 }, // OE 3
-    { country: 'HNG', PI: 'B41F', format: 'png', freq: 96.4 }, // BASE FM
-    { country: 'HNG', PI: 'B208', format: 'png', freq: 103.3 }, // RETRO
-    { country: 'SVK', PI: '5354', format: 'svg', freq: 88.0 }, // MELODY
-    { country: 'SVK', PI: '521D', format: 'png', freq: 97.0 }, // EXPRES
-    { country: 'HNG', PI: 'B09A', format: 'png', freq: 107.0 }, // LAKIHEGY
-    { country: 'HNG', PI: 'B317', format: 'png', freq: 89.5 }, // RADIO 1
-    { country: 'SVK', PI: '5201', format: 'png', freq: 96.6 }, // SRO 1
-    { country: 'SVK', PI: '534F', format: 'svg', freq: 95.2 }, // EUROPA 2
-    { country: 'AUT', PI: 'AC0C', format: 'png', freq: 89.9 }, // RADIO-W
-    { country: 'HNG', PI: 'B409', format: 'png', freq: 90.9 }, // Jazzy
-    { country: 'HNG', PI: 'B40C', format: 'png', freq: 95.8 }, // SLAGERFM
-    { country: 'HNG', PI: 'B416', format: 'png', freq: 98.0 }, // radiocafe
-    { country: 'SVK', PI: '5340', format: 'png', freq: 94.3 }, // FUNRADIO
-    { country: 'AUT', PI: 'A602', format: 'svg', freq: 97.9 }, // RADIO-N
-    { country: 'HNG', PI: 'B41E', format: 'png', freq: 103.9 }, // ROCK FM
-    { country: 'HNG', PI: 'B413', format: 'png', freq: 98.6 }, // MANNA FM
-    { country: 'HNG', PI: 'B33C', format: 'png', freq: 99.5 }, // BEST FM
-    { country: 'AUT', PI: 'A3FF', format: 'png', freq: 105.8 }  // kronehit
+    { country: 'F', PI: 'F750', format: 'png', freq: 88.6 }, // RVM
+    { country: 'F', PI: 'F22F', format: 'png', freq: 89.7 }, // JAZZ
+    { country: 'F', PI: 'F7B1', format: 'png', freq: 90.6 }, // BOUTON
+    { country: 'F', PI: 'F742', format: 'png', freq: 91.2 }, // RADIO 8
+    { country: 'F', PI: 'F226', format: 'png', freq: 91.8 }, // RIRE
+    { country: 'F', PI: 'F225', format: 'png', freq: 92.6 }, // M RADIO
+    { country: 'F', PI: 'F220', format: 'png', freq: 93.9 }, // NRJ
+    { country: 'F', PI: 'F931', format: 'png', freq: 96.6 }, // CONTACT
+    { country: 'F', PI: 'F215', format: 'png', freq: 97.7 }, // RTL2
+    { country: 'F', PI: 'F221', format: 'png', freq: 98.4 }, // CLASSIQ
+    { country: 'F', PI: 'F214', format: 'png', freq: 99.4 }, // SKYROCK
+    { country: 'F', PI: 'F2E2', format: 'png', freq: 99.9 }, // EUROPE2
+    { country: 'F', PI: 'F207', format: 'png', freq: 100.9 }, // FRANCE BLEU
+    { country: 'F', PI: 'F224', format: 'png', freq: 101.6 }, // CHERIEFM
+    { country: 'F', PI: 'F748', format: 'png', freq: 102.2 },  // CHAPAGNEFM
+    { country: 'F', PI: 'F213', format: 'png', freq: 102.9 },  // EUROPE 1
+    { country: 'F', PI: 'F211', format: 'png', freq: 103.4 },  // RTL
+    { country: 'F', PI: 'F217', format: 'png', freq: 103.9 },  // FUN
+    { country: 'F', PI: 'F218', format: 'png', freq: 105.1 },  // NOSTALGIE
+    { country: 'F', PI: 'F206', format: 'png', freq: 105.9 },  // FRANCE INFO
 ];
 
 // CONFIG END //
